7159b50cd1b11847f0d2294aa20679d78386cc04
mim1q <mim1q.dev@gmail.com>
2023-04-01 19:57:51 +0200

    Helper static functions for creating the BlockStates

--- base/src/main/kotlin/mim1qsdatagen/base/data/blockstate/BlockState.kt
+++ base/src/main/kotlin/mim1qsdatagen/base/data/blockstate/BlockState.kt
@@ -10,4 +10,44 @@ abstract class BlockState : MinecraftData {
   override fun getDefaultOutputPath(baseDir: Path, namespace: String): Path {
     return baseDir.resolve("assets/${namespace}/blockstates/")
   }
+
+  companion object {
+    /**
+     * Creates a simple, single-state Block State
+     *
+     * @param model the model to be used in the Block State
+     * @return a new [VariantBlockState] instance with the specified model assigned to the `""` variant
+     */
+    fun createSingle(model: BlockStateModel): VariantBlockState {
+      return create().variant("", model)
+    }
+
+    /**
+     * Creates a simple, single-state Block State based on the specified model name
+     *
+     * @param modelName the name of the model to be used in the Block State
+     * @return a new [VariantBlockState] instance with the specified model assigned to the `""` variant
+     */
+    fun createSingle(modelName: String): VariantBlockState {
+      return createSingle(BlockStateModel(modelName))
+    }
+
+    /**
+     * Creates a new Block State
+     *
+     * @return a new [VariantBlockState] instance with no specified variants
+     */
+    fun create(): VariantBlockState {
+      return VariantBlockState()
+    }
+
+    /**
+     * Creates a new Multipart Block State
+     *
+     * @return a new [MultipartBlockState] instance with no specified entries
+     */
+    fun createMultipart(): MultipartBlockState {
+      return MultipartBlockState()
+    }
+  }
 }
