4d073b8b4e042cf81ef0a1b7e481af2364ff2668
mim1q <mim1q.dev@gmail.com>
2023-05-05 22:48:19 +0200

    Moved files for a single-project approach

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,40 @@
-import org.gradle.kotlin.dsl.`maven-publish`
-
 plugins {
   java
   `maven-publish`
+  id("org.jetbrains.kotlin.jvm")
+}
+
+repositories {
+  // Use Maven Central for resolving dependencies.
+  mavenCentral()
+}
+
+dependencies {
+  constraints {
+    // Define dependency versions as constraints
+    implementation("org.apache.commons:commons-text:1.9")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  }
+
+  // Align versions of all Kotlin components
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+  // Use the Kotlin JDK 8 standard library.
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+  // Align versions of all Kotlin components
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+  // GSON for JSON file handling
+  implementation("com.google.code.gson:gson:2.10.1")
+
+  // Use JUnit Jupiter for testing.
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+}
+
+tasks.named<Test>("test") {
+  // Use JUnit Platform for unit tests.
+  useJUnitPlatform()
 }
 
 publishing {
--- lib/build.gradle.kts
+++
@@ -1,3 +0,0 @@
-plugins {
-  id("tada.kotlin-library-conventions")
-}
